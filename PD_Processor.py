5ca2bd285af947b62d89eefc1b2ebfc2eaaf2bce
mph759 <mhassett@iinet.net.au>
2023-10-05 11:32:02 +1100

    Added basic conversion factor for the x dimension of the data (e.g. 2theta to q), and the option to save rather than plot the figures directly

--- PD_Processor.py
+++ PD_Processor.py
@@ -12,9 +12,16 @@ import re
 from pathlib import Path
 import pandas as pd
 
+
 class PDDatset:
 
-    def __init__(self, dataset_root: str, target_dataset: str, target_subtag: str = '', extension: str = '.xye'):
+    def __init__(self,
+                 dataset_root: str,
+                 target_dataset: str,
+                 target_subtag: str = '',
+                 conversion_factor=None,
+                 x_dim=f'2$\Theta$ / $^\circ$',
+                 extension: str = '.xye'):
         '''
 
         :param dataset_root: Primary directory to where general data to be ingested is stored
@@ -26,6 +33,8 @@ class PDDatset:
         self.target_dataset = target_dataset
         self.target_subtag = target_subtag
         self.ext = extension
+        self.conversion_factor = conversion_factor
+        self.x_dim = x_dim
         self.manifest = []
         self.__get_manifest__()
         # print(self.manifest)
@@ -36,7 +45,7 @@ class PDDatset:
         :return: A list of all found files as Path objects
         '''
         raw_list = sorted(Path(f'{self.dataset_root}\\{self.target_dataset}').glob(f'*{self.target_subtag}*{self.ext}'))
-        # print(Path(self.dataset_root + '\\' + self.target_dataset))
+        # print(Path(f'{self.dataset_root}\\{self.target_dataset}'))
         # print(raw_list)
         convert = lambda text: int(text) if text.isdigit() else text
         alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', str(key))]
@@ -52,21 +61,31 @@ class PDDatset:
         '''
         xye_array = np.loadtxt(self.manifest[pattern_index])
         # plt.figure()
-        plt.xlabel('2$\\theta$')
+        plt.xlabel(self.x_dim)
         plt.ylabel('Intensity')
         # plt.plot(xye_array[:, 0], np.log10(xye_array[:, 1]), linewidth=0.5)
         plt.plot(xye_array[:, 0], xye_array[:, 1], linewidth=0.2)
 
-    def generate_waterfall(self, step: int = 1000, type: str = "normal") -> object:
+    def generate_waterfall(self,
+                           step: int = 1000,
+                           type: str = "normal",
+                           save: bool = False,
+                           save_dir: object = Path.cwd(),
+                           save_filename: str = 'foo'):
         '''
         Generate a plot of all patterns in the manifest on a single figure, with a spacing step between each plot
         :param step: The step amount for the gap between line plots
         :param type: Type of treatment to data before plotting. "normal" will plot as-is, "log" will plot log10 of the data
+        :param save: Boolean of whether you want to save the file (True), or plot directly (False)
+        :param save_dir: Directory to which you will save the figure
+        :param save_filename: File name for the figure
         :return:
         '''
         for indx, pattern in enumerate(self.manifest):
             try:
                 xye_array = np.loadtxt(pattern)
+                if self.conversion_factor is not None:
+                    xye_array[:, 0] = np.array(map(self.conversion_factor, xye_array[:, 0]))
             except:
                 xye_array = np.loadtxt(pattern, skiprows=1)
             xye_array[:, 1] += (indx + 1) * step
@@ -75,15 +94,23 @@ class PDDatset:
             if type == "log":
                 plt.plot(xye_array[:, 0], np.log10(xye_array[:, 1]), linewidth=1)
             # else:
-                # raise ValueError("type must be 'normal' or 'log'")
+            # raise ValueError("type must be 'normal' or 'log'")
             plt.title(f'{pattern}')
         plt.legend()
-        plt.xlabel('2$\\theta$')
+        plt.xlabel(self.x_dim)
         plt.ylabel('Intensity')
+        plt.ylim(0, np.median(xye_array[:, 1]) + 4 * step)
         plt.gca().get_yaxis().set_ticks([])
-        plt.show()
+        if save:
+            plt.savefig(f'{save_dir}/{save_filename}')
+        else:
+            plt.show()
 
-    def generate_heatmap(self, temp_range: list[float], lim: int = 20000):
+    def generate_heatmap(self, temp_range: list[float],
+                         lim: int = 20000,
+                         save: bool = False,
+                         save_dir=Path.cwd(),
+                         save_filename: str = 'foo'):
         """
         Parameters
         ----------
@@ -98,15 +125,21 @@ class PDDatset:
         print(f'wf_array.shape {wf_array.shape}')
         for indx, pattern in enumerate(self.manifest):
             xye_array = np.loadtxt(pattern)[:lim]
+            if self.conversion_factor is not None:
+                xye_array[:, 0] = np.array(map(self.conversion_factor, xye_array[:, 0]))
             xye_int_slice = xye_array[:, 1]
             wf_array[:, indx] = xye_int_slice
         temp_range.sort()
         shape = [0, xye_array[lim - 1, 0], temp_range[0], temp_range[1]]
         plt.figure()
-        plt.imshow(np.transpose(wf_array), aspect=0.1, extent=shape, origin='lower')
-        plt.xlabel('2$\Theta$ / $^\circ$')
+        plt.imshow(np.transpose(wf_array), aspect=0.1, extent=shape, origin='lower', vmax=50000)
+        plt.xlabel(self.x_dim)
         plt.ylabel('Run No.')
-        plt.show()
+        plt.tight_layout
+        if save:
+            plt.savefig(f'{save_dir}/{save_filename}.jpg', bbox_inches="tight")
+        else:
+            plt.show()
 
     def ensemble_csv_convert(self):
         xye_list = Path(f'{self.dataset_root}\\').glob(f'**\\*.xye')
@@ -119,7 +152,10 @@ class PDDatset:
             print(new_root)
             np.savetxt(new_root, arr, delimiter=',')
 
-    def inspect_multi_pattern(self, title: str, x_range: list[float] = [], twotheta: bool = True):
+    def inspect_multi_pattern(self,
+                              title: str,
+                              x_range: list[float] = [],
+                              twotheta: bool = True):
         '''
         Method for plotting all single diffraction patterns from the manifest directory.
 
@@ -152,15 +188,36 @@ class PDDatset:
 
 if __name__ == '__main__':
     print('Powder Diffraction Processing')
-    dataset_root = (r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Synchrotron Beamtime\Data')   # The main directory; leave the last folder off
-    target_dataset = r'TEAF\TEAF2\CRYST'  # The subdirectory where the exact data is
+    dataset_root = (
+        r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Synchrotron Beamtime\Data')  # The main directory; leave the last folder off
+    target_dataset = [r'DEAF\DEAFwool2\VT',
+                      r'EAA\EAA\VT',
+                      r'EAF\EAF_2',
+                      r'EAG\EAGwool2\CRYST',
+                      r'EAL\EALwool\CRYST',
+                      r'EAN\EAN\VT2',
+                      r'EATFA\EATFA\VT',
+                      r'TEAF\TEAFwool\CRYST']  # The subdirectory where the exact data is
     target_subtag = 'p12_ns'  # Text in the listed file. Can be left blank
     file_ext = '.xye'  # File extension for files to be searched for
-    # wavelength = 0.826278E-10  # wavelength in metres. Only needed if converting from 2theta to q
-    dset = PDDatset(dataset_root, target_dataset, target_subtag, extension=file_ext)
 
-    dset.generate_waterfall(step=5000, type="log")
-    dset.generate_heatmap()
+    wavelength = 0.826278E-10  # wavelength in metres. Only needed if converting from 2theta to q
+    # convert_2theta_to_q = lambda array: ((4 * np.pi) / wavelength) * np.sin(np.deg2rad(array) / 2)
+
+    for target in target_dataset:
+        filename = target.split("\\")[0]
+        dset = PDDatset(dataset_root, target, target_subtag)
+        # , conversion_factor=convert_2theta_to_q, x_dim='q', extension=file_ext)
+        dset.generate_waterfall(step=10000, type="normal",
+                                save=True,
+                                save_dir=r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Paper\Figures\Waterfall plots\Normal',
+                                save_filename=f'{filename}_normalwaterfall')
+
+        dset.generate_heatmap(temp_range=[120, 310], lim=10000,
+                              save=True,
+                              save_dir=r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Paper\Figures\Waterfall plots\Colour map',
+                              save_filename=f'{filename}_2Dwaterfall')
+
     # dset.generate_waterfall(step=5000, type="normal")
     # dset.ensemble_csv_convert()
     # dset.inspect_pattern(0)
