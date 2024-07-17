6fa2f19caab28da248d12a7b214ced636544247b
Michael Hassett <michael@hassett.net.au>
2024-07-17 20:56:18 +1000

    Improved ref_result_grabber.py, to always get the results whether the space group is centrosymmetric or not, and to use pandas Dataframes

--- ref_result_grabber.py
+++ ref_result_grabber.py
@@ -7,25 +7,34 @@ Created: 2024-05-14
 
 from pprint import pprint
 import re
+from typing import Tuple
+
 import pandas as pd
+import numpy as np
 from pathlib import Path
+from uncertainties import ufloat
 
 
-def get_fitting_params(ref_file: Path):
+def get_fitting_params(ref_file: Path) -> dict:
     with open(ref_file, 'r') as f:
         for line in f:
             if line.startswith('|GOF'):
+                print('Found GOF')
                 results = line.strip('|\s+\n')
                 results = results.split(' ')
                 results = list(filter(None, results))
-                results = [x for x in results if x != '=']
+                results = [x.strip('=') for x in results if x != '=']
+
                 for i in range(1, len(results), 2):
                     results[i] = float(results[i])
                 res_dct = {results[i]: results[i + 1] for i in range(0, len(results), 2)}
                 return res_dct
+    print('No GOF found')
 
 
-def get_unit_cell_params(ref_file: Path):
+def get_unit_cell_params(ref_file: Path) -> pd.DataFrame:
+    if not ref_file:
+        return None
     with open(ref_file, 'r') as f:
         last_line = len(f.readlines())
     header_line = last_line
@@ -40,46 +49,82 @@ def get_unit_cell_params(ref_file: Path):
                 break
     cell_params = pd.read_csv(ref_file, sep='\s+', skiprows=end_line - 2, header=None,
                               names=['a', 'b', 'c', 'alpha', 'beta', 'gamma', 'volume', 'density'], nrows=2)
-    cell_params.rename(index=dict(zip(cell_params.index,['value', 'error'])), inplace=True)
+    cell_params.rename(index=dict(zip(cell_params.index, ['value', 'error'])), inplace=True)
+    full_cell_params = {column_name: (f'{ufloat(value, error):0.1uS}' if error else f'{int(value)}') for
+                        column_name, (value, error) in cell_params.items()}
+    return full_cell_params
 
-    return cell_params
 
-
-def get_space_group(ref_file: Path):
+def get_space_group(ref_file: Path) -> tuple[None, None] | tuple[str, int]:
+    if not ref_file:
+        return None, None
     with open(ref_file, 'r') as f:
         for line in f:
-            if line.startswith('Centrosymmetric space group'):
+            if 'centrosymmetric space group' in line.lower():
                 space_group = line.strip('\n')
                 space_group = space_group.split(' ')[3:]
                 space_group = (space_group[0], int(space_group[-1]))
                 return space_group
 
 
+def get_best_dir(data_folder) -> tuple:
+    current_best = None
+    ref_files = list(data_folder.glob('**/*.ref'))
+    if len(list(ref_files)) == 0:
+        print('No .ref files found')
+        return None, None
+    else:
+        print(f'Found {len(list(ref_files))} refs for {data_folder}')
+    results = {}
+    for ref_file in ref_files:
+        print(ref_file)
+        results[ref_file] = get_fitting_params(ref_file)
+        if results[ref_file] is None:
+            continue
+        if current_best is None:
+            current_best = ref_file
+        elif results[ref_file]['wRp'] < results[current_best]['wRp']:
+            current_best = ref_file
+    if current_best is None:
+        return None, None
+    print(results)
+    best_file = current_best
+    best_wRp = results[best_file]['wRp']
+    return best_file, best_wRp
+
+
+def get_all_data(data: pd.Series) -> pd.Series:
+    data['file'], data['wRp'] = get_best_dir(data['file'] / data['sample'])
+    if data['file'] is None:
+        print(f'No data found in refs for {data["sample"]}')
+        return data
+    try:
+        space_group = get_space_group(data['file'])
+        data['space group'], data['space group no'] = space_group
+
+        data.update(get_unit_cell_params(data['file']))
+    except TypeError:
+        print('No unit cell found')
+        return data
+
+    return data
+
+
 if __name__ == '__main__':
-    base_dir = Path(
-        r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Data '
-        r'Analysis\Jack\pil-xrd\Binns_17649')
-    samples = ['AMAMS', 'AMAN', 'BAMS', 'EATFA/EATFA/ht_refinement']
-    for sample in samples:
-        current_best = None
-        sample_folder = base_dir / sample
-        ref_files = sample_folder.glob('*.ref')
-        results = {}
-        for ref_file in ref_files:
-            results[ref_file.name] = get_fitting_params(ref_file)
-            if results[ref_file.name] is None:
-                continue
-            if current_best is None:
-                current_best = ref_file.name
-            elif results[ref_file.name]['wRp'] < results[current_best]['wRp']:
-                current_best = ref_file.name
-        best_file = sample_folder / current_best
-        print(f"Best wRp for {sample} is {results[current_best]['wRp']} from {current_best}")
 
-        unit_cell_params = get_unit_cell_params(best_file)
-        space_group = get_space_group(best_file)
+    # base_dir = Path(r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Data Analysis\Jack\pil-xrd\Binns_17649')
+    # samples = ['AMAMS', 'AMAN', 'BAMS', 'EATFA', 'BAA', 'BAN', 'EACl', 'EAMS', 'EAPC', 'EAPH', 'EtAMS', 'EtAPH']
 
-        print(space_group)
-        print(unit_cell_params)
-        print('\n')
+    base_dir = Path(r'C:\Users\Michael_X13\OneDrive - RMIT University\Research\2021 - Honours Research Project\Data Analysis')
+    # samples = ['DEAF/alpha', 'DEAF/beta', 'BAA', 'EAMS', 'EAN/alpha', 'EAN/beta', 'EATFA/alpha', 'EATFA/beta']
+    samples = ['EAA', 'EACl']
+    df = pd.DataFrame({'sample': samples, 'file': base_dir, 'wRp': np.nan,
+                       'space group': None, 'space group no': np.nan,
+                       'a': np.nan, 'b': np.nan, 'c': np.nan,
+                       'alpha': np.nan, 'beta': np.nan, 'gamma': np.nan,
+                       'volume': np.nan, 'density': np.nan})
 
+    df = df.apply(get_all_data, axis=1, result_type='broadcast')
+
+    output_dir = Path.cwd() / 'output'
+    df.to_csv(output_dir / 'ref_data_v3.csv', index=False, encoding='utf-8')
